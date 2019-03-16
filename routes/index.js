bec6bff0c45852e1a052e1e8d3d3f3afda306a27
Nick Bohannan <nbohannan@gmail.com>
2019-03-16 17:55:18 -0500

    first push to heroku

--- routes/index.js
+++ routes/index.js
@@ -1,24 +1,27 @@
-const express = require('express');
-const router = express.Router();
-const User = require('../models/index');
-const nodemailer = require('nodemailer')
+const express = require('express')
+const client = require('twilio')(accountSid, authToken)
+
+const User = require('../models/index')
+const mailer = require('../lib/mailer')
+
+const accountSid = process.env.TWILIO_ACCOUNT_SID
+const authToken = process.env.TWILIO_AUTH_TOKEN
+
+const router = express.Router()
 
 router.get('/', (req, res) => {
     res.render('index')
 })
 
-router.post('/', (req, res) => {
-    // gather twilio info and require
-    const accountSid = process.env.TWILIO_ACCOUNT_SID
-    const authToken = process.env.TWILIO_AUTH_TOKEN
-    const client = require('twilio')(accountSid, authToken)
+router.post('/', async (req, res) => {
 
-    // main query
-    User.findAll({
-        where: {
-            zipcode1: req.body.zip
-        }
-    }).then((users) => {
+    try {
+        // main query
+        let users = await User.findAll({
+            where: {
+                zipcode1: req.body.zip
+            }
+        })
 
         // send if no matches
         if (users.length === 0) {
@@ -31,91 +34,50 @@ router.post('/', (req, res) => {
             req.body.add1 + " " +
             req.body.add2 + " " +
             req.body.city + ", " +
-            req.body.state + ". You can reach this individual at: " +
+            req.body.state + " " +
+            req.body.zip + ". You can reach this individual at: " +
             req.body.phone
 
         // send message to each recipient
-        users.forEach((user) => {
-            client.messages
+        users.forEach(async user => {
+            let message = await client.messages
                 .create({
                     body: bodyString,
                     from: process.env.TWILIO_NUM,
                     to: user.phone
                 })
-                .then(message => console.log(message.sid))
-        }
+            console.log(message.sid)
+        })
 
-        //define mailer function
-        async function main() {
-            let transporter = nodemailer.createTransport({
-                host: "smtp.gmail.com",
-                port: 465,
-                secure: true,
-                auth: {
-                    user: process.env.EMAIL,
-                    pass: process.env.EMAIL_PASS
-                }
-            })
+        mailer(users, req)
 
-            let recipients
+    } catch(error) {
+        console.error(error)
+    }
 
-            // this handles both single and multiple recipients
-            if (users.length > 1) {
-                let emailArray = []
-                users.forEach((user) => {
-                    emailArray.push(user.email)
-                })
-                recipients = emailArray.join(', ')
-            } else {
-                recipients = users[0].email
-            }
-
-            // define mail options
-            let mailOptions = {
-                from: '"naloxone911" <sender@naloxone911.com>', // sender address
-                to: recipients, // list of receivers
-                subject: "(TEST) ALERT - NALOXONE NEEDED IN YOUR AREA", // Subject line
-                text: "test email", // plain text body
-                html: "<b>THIS IS A TEST - ALERT - NALOXONE NEEDED AT "
-                    + req.body.add1 + " "
-                    + req.body.add2 + " "
-                    + req.body.city + ", "
-                    + req.body.state + " "
-                    + req.body.zip + ". You can call this individual at "
-                    + req.body.phone + "</b>" // html body
-            }
-
-            //send message
-            let info = await transporter.sendMail(mailOptions)
-            console.log("Message sent: %s", info.messageId)
-        }
-
-        // invoke mailer function
-        main().catch(console.error)
-
-    }).then(() => {
-        // send response page, actually make a page tho homie
-        res.send('<h1>Message sent to at least one responder. Please wait</h1>')
-    }).catch(console.error)
+    // send response page, actually make a page tho homie
+    res.send('<h1>Message sent to at least one responder. Please wait</h1>')
 })
 
 router.get('/signup', (req, res) => {
     res.render('signup')
 })
 
-router.post('/signup', (req, res, next) => {
+router.post('/signup', async (req, res) => {
     // create user in the db then send thank you page
-    User.create({
-        email: req.body.email,
-        phone: req.body.phone,
-        zipcode1: req.body.zip1,
-        zipcode2: req.body.zip2,
-        zipcode3: req.body.zip3
-    }).then(() => {
+    try {
+        await User.create({
+            email: req.body.email,
+            phone: req.body.phone,
+            zipcode1: req.body.zip1,
+            zipcode2: req.body.zip2,
+            zipcode3: req.body.zip3
+        })
+
         res.render('thankyou')
-    }).catch((err) => {
-        console.log(err)
-    })
+    } catch(error) {
+        console.log(error)
+    }
 })
 
 module.exports = router
